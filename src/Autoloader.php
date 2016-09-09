3822da962f0c2c1647fd13ce2adf55ca7c9797f8
Loic Pedra <loic.pedra@gmail.com>
2016-09-09 11:23:02 +0200

    Create Autoloader

---
+++ src/Autoloader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LitApi;
+
+class Autoloader {
+
+	static function register()
+	{
+		spl_autoload_register(array(__CLASS__, 'autoload'));
+	}
+
+	static function autoload($class)
+	{
+		$class = str_replace(__NAMESPACE__ . "\\", '', $class);
+		$class = str_replace("\\", '/', $class);
+		require 'src/' . $class . '.php';
+	}
+
+}
+
+Autoloader::register();
+
+?>
